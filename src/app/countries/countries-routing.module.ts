510a75fbf3f9075971022826b8cf91524b12cbe8
Alexander Diaz <aliensanderdiaz@gmail.com>
2023-07-02 00:25:21 -0500

    sección terminada

---
+++ src/app/countries/countries-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { SelectorPageComponent } from './pages/selector-page/selector-page.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      { path: '', component: SelectorPageComponent },
+      { path: '**', redirectTo: '' },
+    ],
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class CountriesRoutingModule {}
